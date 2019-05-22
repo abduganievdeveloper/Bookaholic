2e2e31146a25262638c5be40c2ffb2e480ae92f6
AbdumalikFront <iwillstudyingermany@gmail.com>
2019-05-22 16:01:59 +0500

    Updates

---
+++ precache-manifest.b769c6f03e8a7c75a86584fdaff67864.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58caa4072fd16de234ea2f8bb9cbab14",
+    "url": "/bookaholicp/index.html"
+  },
+  {
+    "revision": "0120f79e002046fc7be5",
+    "url": "/bookaholicp/static/css/main.a4f515b5.chunk.css"
+  },
+  {
+    "revision": "3d529cbf2ea4ab7b6398",
+    "url": "/bookaholicp/static/js/2.2f26353c.chunk.js"
+  },
+  {
+    "revision": "0120f79e002046fc7be5",
+    "url": "/bookaholicp/static/js/main.9c4f760a.chunk.js"
+  },
+  {
+    "revision": "829b48fab8f940e5e728",
+    "url": "/bookaholicp/static/js/runtime~main.49bc5d7a.js"
+  }
+]);
